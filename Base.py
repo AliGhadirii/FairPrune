ec7812ab83d5c871512d3576c5e12b5388b055d7
Ali Ghadiri Modarres <z5485577@ad.unsw.edu.au>
2023-09-14 10:40:54 +1000

    Eval model added

--- Base.py
+++ Base.py
@@ -36,7 +36,7 @@ def train_model(
     start_epoch = 0
 
     best_model_path = os.path.join(
-        config["default"]["output_path"], "Resnet18_checkpoint_BASE.pth"
+        config["default"]["output_folder_path"], "Resnet18_checkpoint_BASE.pth"
     )
 
     if best_model_path.is_file():
@@ -204,6 +204,116 @@ def train_model(
     return model, training_results, validation_results
 
 
+def eval_model(model, dataloaders, dataset_sizes, device, level, config):
+    model = model.eval()
+    prediction_list = []
+    fitzpatrick_list = []
+    hasher_list = []
+    labels_list = []
+    p_list = []
+    topk_p = []
+    topk_n = []
+    d1 = []
+    d2 = []
+    d3 = []
+    p1 = []
+    p2 = []
+    p3 = []
+    with torch.no_grad():
+        running_corrects = 0
+        running_balanced_acc_sum = 0
+        total = 0
+        for batch in dataloaders["val"]:
+            inputs = batch["image"].to(device)
+            classes = batch[level]
+            fitzpatrick = batch["fitzpatrick"]
+            classes = torch.from_numpy(np.asarray(classes)).to(device)
+            fitzpatrick = torch.from_numpy(np.asarray(fitzpatrick)).to(device)
+            hasher = batch["hasher"]
+
+            outputs = model(inputs.float())  # (batchsize, classes num)
+            probability = torch.nn.functional.softmax(outputs[0], dim=1)
+            ppp, preds = torch.topk(probability, 1)  # topk values, topk indices
+            if level == "low":
+                _, preds5 = torch.topk(probability, 3)  # topk values, topk indices
+                # topk_p.append(np.exp(_.cpu()).tolist())
+                topk_p.append((_.cpu()).tolist())
+                topk_n.append(preds5.cpu().tolist())
+            running_corrects += torch.sum(preds.reshape(-1) == classes.data)
+            running_balanced_acc_sum += (
+                balanced_accuracy_score(classes.data.cpu(), preds.reshape(-1).cpu())
+                * inputs.shape[0]
+            )
+            p_list.append(ppp.cpu().tolist())
+            prediction_list.append(preds.cpu().tolist())
+            labels_list.append(classes.tolist())
+            fitzpatrick_list.append(fitzpatrick.tolist())
+            hasher_list.append(hasher)
+            total += inputs.shape[0]
+        acc = float(running_corrects) / float(dataset_sizes["val"])
+        balanced_acc = float(running_balanced_acc_sum) / float(dataset_sizes["val"])
+
+    if level == "low":
+        for j in topk_n:  # each sample
+            for i in j:  # in k
+                d1.append(i[0])
+                d2.append(i[1])
+                d3.append(i[2])
+        for j in topk_p:
+            for i in j:
+                # print(i)
+                p1.append(i[0])
+                p2.append(i[1])
+                p3.append(i[2])
+
+        def flatten(list_of_lists):
+            if len(list_of_lists) == 0:
+                return list_of_lists
+            if isinstance(list_of_lists[0], list):
+                return flatten(list_of_lists[0]) + flatten(list_of_lists[1:])
+            return list_of_lists[:1] + flatten(list_of_lists[1:])
+
+        df_x = pd.DataFrame(
+            {
+                "hasher": flatten(hasher_list),
+                "label": flatten(labels_list),
+                "fitzpatrick": flatten(fitzpatrick_list),
+                "prediction_probability": flatten(p_list),
+                "prediction": flatten(prediction_list),
+                "d1": d1,
+                "d2": d2,
+                "d3": d3,
+                "p1": p1,
+                "p2": p2,
+                "p3": p3,
+            }
+        )
+    else:
+        df_x = pd.DataFrame(
+            {
+                "hasher": flatten(hasher_list),
+                "label": flatten(labels_list),
+                "fitzpatrick": flatten(fitzpatrick_list),
+                "prediction_probability": flatten(p_list),
+                "prediction": flatten(prediction_list),
+            }
+        )
+
+    num_epoch = config["default"]["n_epochs"]
+    df_x.to_csv(
+        os.path.join(
+            config["default"]["output_folder_path"],
+            f"validation_results_Resnet18_{num_epoch}_random_holdout_BASE.csv",
+        ),
+        index=False,
+    )
+    print(
+        "\n Final Validation results: Accuracy: {}  Balanced Accuracy: {} \n".format(
+            acc, balanced_acc
+        )
+    )
+
+
 def main(config):
     print("CUDA is available: {} \n".format(torch.cuda.is_available()))
     print("Starting... \n")
@@ -214,7 +324,7 @@ def main(config):
     dataloaders, dataset_sizes, num_classes = get_fitz17k_dataloaders(
         root_image_dir=config["root_image_dir"],
         Generated_csv_path=config["Generated_csv_path"],
-        level="high",
+        level=config["default"]["level"],
         binary_subgroup=True,
         holdout_set="random_holdout",
         seed=42,
@@ -241,6 +351,26 @@ def main(config):
         exp_lr_scheduler,
         device,
         config,
+    )
+
+    num_epoch = config["default"]["n_epochs"]
+    training_results.to_csv(
+        os.path.join(
+            config["default"]["output_folder_path"],
+            f"training_results_Resnet18_{num_epoch}_random_holdout_BASE.csv",
+        ),
+        index=False,
+    )
+    validation_results.to_csv(
+        os.path.join(
+            config["default"]["output_folder_path"],
+            f"all_validation_results_Resnet18_{num_epoch}_random_holdout_BASE.csv",
+        ),
+        index=False,
+    )
+
+    eval_model(
+        model, dataloaders, dataset_sizes, device, config["default"]["level"], config
     )
 
 
